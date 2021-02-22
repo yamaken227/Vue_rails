c17ccc0f0e5d7f31edcdd6b8294427a77b9ad8b6
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-02-22 19:00:40 +0900

    API作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # APIコントローラへのルーティング
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show]
+    end
+  end
 end
